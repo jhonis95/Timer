ae175b46d88fc0bbd6973fb0e700a8a0205cf26b
jonatan de oliveira <j.jhonis95@gmail.com>
2022-04-30 13:05:20 -0400

    finishing the class timer

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-class Timer{
-    constructor(durationInput,startButton,pauseButton){
-       this.durationInput=durationInput;
-       this.startButton=startButton;
-       this.pauseButton=pauseButton;
-
-       this.startButton.addEventListener('click',this.start);
-       this.pauseButton.addEventListener('click',this.pause);
-    }
-    start=()=>{// usind arrow function to define the right reference to "this"
-        this.tick()
-        this.interval=setInterval(this.tick,1000)//the vareable this.interval now can be use cross methods
-    }
-    pause=()=>{
-        clearInterval(this.interval)
-    }
-    onDurationChange(){
-
-    }
-    tick=()=>{
-        const timeRmaining=parseFloat(this.durationInput.value)//parseFloat is to transfor the string to a number
-        this.durationInput.value=timeRmaining-1;
-    }
-    getTime(){
-        return parseFloat(this.durationInput.value)
-    }
-    setTime(time){
-        this.durationInput.value=time
-    }
-}
 const durationInput=document.getElementById("duration")
 const startButton=document.getElementById("start")
 const pauseButton=document.getElementById("pause")
 
-const timer= new Timer(durationInput,startButton,pauseButton)
+const timer= new Timer(durationInput,startButton,pauseButton,{
+    onStart(){
+        console.log("Time started")
+    },
+    onTick(){
+        console.log("Timer just ticked down")
+    },
+    onComplete(){  
+        console.log("Timer is completed")
+    }
+})
---
+++ timer.js
@@ -0,0 +1,50 @@
+class Timer{
+    constructor(durationInput,startButton,pauseButton,callbacks){
+        //callbacks is the parameter that have callbacks that allow us to track/notifie outside what is happen in the class
+       this.durationInput=durationInput;
+       this.startButton=startButton;
+       this.pauseButton=pauseButton;
+        if(callbacks){
+            this.onStart=callbacks.onStart;
+            this.onTick=callbacks.onTick;
+            this.onComplete=callbacks.onComplete;
+        }
+       
+       this.startButton.addEventListener('click',this.start);
+       this.pauseButton.addEventListener('click',this.pause);
+    }
+    start=()=>{// usind arrow function to define the right reference to "this"
+        if(this.onStart){
+            this.onStart()
+        }        
+        this.tick()
+        this.interval=setInterval(this.tick,1000)//the vareable this.interval now can be use cross methods
+    }
+    pause=()=>{
+        clearInterval(this.interval)
+    }
+    onDurationChange(){
+
+    }
+    tick=()=>{
+        if(this.timeRemaining<=0){//make sure that our timeRemaining is not bellow 0
+            this.pause();
+            if(this.onComplete){
+                this.onComplete();
+            }
+        }else{
+            this.timeRemaining=this.timeRemaining - 1;// here we are using get and set
+        //set timeRemaining()   get timeRemaining()    
+        //get will return the value in float and set will set that value to the input
+            if(this.onTick){
+                this.onTick();
+            }
+        }
+    }
+    get  timeRemaining(){//with get wecan use the method as a value
+        return parseFloat(this.durationInput.value)//parseFloat is to transfor the string to a number
+    }
+    set timeRemaining(time){ //setting the variable
+        this.durationInput.value=time
+    }
+}
